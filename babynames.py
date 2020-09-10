a4e0a84bafd5f5286e661062bd56abb8a4141223
Tracy DeWitt <Justdewitt8485@gmail.com>
2020-09-10 16:08:26 -0500

    all tests passing, also had to create space in replace statments for them to work correctly

--- babynames.py
+++ babynames.py
@@ -30,6 +30,9 @@ Suggested milestones for incremental development:
  - Build the [year, 'name rank', ... ] list and print it
  - Fix main() to use the extracted_names list
 """
+__author__ = '''Tracy DeWitt
+Mike Boring'''
+
 
 import sys
 import re
@@ -44,8 +47,33 @@ def extract_names(filename):
     ['2006', 'Aaliyah 91', 'Aaron 57', 'Abagail 895', ...]
     """
     names = []
-    # +++your code here+++
-    return names
+    with open(filename) as f:
+        text = f.read()
+        year = r'Popularity\sin\s(\d\d\d\d)'
+        y = re.search(year, text)
+        names.append(
+            text[y.start():y.end()].replace('Popularity in ', ''))
+        for text_section in text.split():
+            if text_section.startswith('align="right"><td>'):
+                names.append(
+                    text_section.replace('align="right"><td>', ' ')
+                    .replace('</td><td>', ' ').replace('</td>', ' '))
+    final_list = []
+    for single_name in names:
+        if single_name == names[0]:
+            final_list.append(single_name)
+        else:
+            split_single_name = single_name.split()
+            if len(split_single_name) == 3:
+                if split_single_name[1] + ' ' not in ' '.join(final_list):
+                    final_list.append(
+                        split_single_name[1] + ' ' + split_single_name[0])
+                if split_single_name[2] + ' ' not in ' '.join(final_list):
+                    final_list.append(
+                        split_single_name[2] + ' ' + split_single_name[0])
+                continue
+    final_sorted_list = sorted(final_list)
+    return final_sorted_list
 
 
 def create_parser():
@@ -82,7 +110,17 @@ def main(args):
     # Use the create_summary flag to decide whether to print the list
     # or to write the list to a summary file (e.g. `baby1990.html.summary`).
 
-    # +++your code here+++
+    if create_summary:
+        for name_list in file_list:
+            with open(name_list + '.summary', 'w') as f:
+                new_list = extract_names(name_list)
+                for name in new_list:
+                    f.write(name + '\n')
+    else:
+        for name_list in file_list:
+            new_list = extract_names(name_list)
+            for name in new_list:
+                print(name)
 
 
 if __name__ == '__main__':
